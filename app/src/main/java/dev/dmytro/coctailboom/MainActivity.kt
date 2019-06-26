737083ea0a5c5b39913b15a9cb0d9143a29b9de0
Dmytro Borysov <dimoninvoker@gmail.com>
2019-06-26 22:55:05 +0300

    initial commit

---
+++ app/src/main/java/dev/dmytro/coctailboom/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.dmytro.coctailboom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
